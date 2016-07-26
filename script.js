f8d81d1433c8aebcf14cf3048c9d9a829c04a933
Benjamin <benjaminvanzwienen@live.nl>
2016-07-26 23:37:59 +0200

    Add possibility to change certain icons on click

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ $(window).load(function(){
       buttonHeight: 0,
       buttonWidth: 0,
       appliedPadding: 0,
+      specialButtonState: false,
       init: function(){
         this.events();
         this.setSizeOfSections();
@@ -34,6 +35,14 @@ $(window).load(function(){
           self.setSizeOfSections();
           self.setSizeOfButtons();
           self.alignTextInButtons();
+        });
+
+        // Button pressed
+        $(".btn-elem").on('click', function(){
+          var val = $(this).data("val");
+          if(val === "extra"){
+            self.changeSpecialButtons();
+          }
         });
       },
       checkState: function(){
@@ -67,7 +76,21 @@ $(window).load(function(){
         this.appliedPadding = 7 * padding;
 
         $(".btn-elem").css("padding-top", padding + "px");
+      },
+      changeSpecialButtons: function(){
+        if(this.specialButtonState){
+          this.specialButtonState = false;
+          $(".btn-elem[data-val='extra']").css("color", "black");
+          $("#special-buttons .btn-elem span:nth-child(1)").css("display", "inline");
+          $("#special-buttons .btn-elem span:nth-child(2)").css("display", "none");
+        }else{
+          this.specialButtonState = true;
+          $(".btn-elem[data-val='extra']").css("color", "rgba(50,50,255,1)");
+          $("#special-buttons .btn-elem span:nth-child(2)").css("display", "inline");
+          $("#special-buttons .btn-elem span:nth-child(1)").css("display", "none");
+        }
       }
+
     };
 
     Calculator.init();
